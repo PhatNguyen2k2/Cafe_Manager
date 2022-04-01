593c4535d433dc257f8ce669fe59ae3cfd24aaff
nguye <ngphat1211@gmail.com>
2022-04-01 23:01:55 +0700

    build class bill

---
+++ Cafe_Manager_BE/src/BE/Bill.java
@@ -0,0 +1,68 @@
+package BE;
+
+import java.util.Scanner;
+
+public class Bill {
+	private String id;
+	private int hour;
+	private int minute;
+	private String C_id;
+	private String E_id;
+	private float price;
+	public Bill() {
+		super();
+		id = "";
+		hour = 0;
+		minute = 0;
+		C_id = "";
+		E_id = "";
+		price = 0;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getHour() {
+		return hour;
+	}
+	public void setHour(int hour) {
+		this.hour = hour;
+	}
+	public int getMinute() {
+		return minute;
+	}
+	public void setMinute(int minute) {
+		this.minute = minute;
+	}
+	public String getC_id() {
+		return C_id;
+	}
+	public void setC_id(String c_id) {
+		C_id = c_id;
+	}
+	public String getE_id() {
+		return E_id;
+	}
+	public void setE_id(String e_id) {
+		E_id = e_id;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public void input() {
+		Scanner sc = new Scanner(System.in);
+		id = sc.nextLine();
+		C_id = sc.nextLine();
+		E_id = sc.nextLine();
+		hour = sc.nextInt();
+		minute = sc.nextInt();
+	}
+	public void print() {
+		System.out.println("Id: "+id+", "+hour+"h"+minute+", C_id: "+C_id+", E_id: "+E_id+", Price: "+price);
+	}
+}
